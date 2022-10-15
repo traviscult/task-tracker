2108466c3659e6803321b360a45e6a43a60802b7
traviscult <60018617+traviscult@users.noreply.github.com>
2022-10-15 17:29:04 -0500

    created header component and imported to app js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import { Header } from "./components/Header";
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello From React</h1>
+      <Header />
     </div>
   );
 }
