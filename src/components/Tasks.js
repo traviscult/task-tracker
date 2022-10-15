89165aa600a94408ef4a072a1aaf63f5616cfa41
traviscult <60018617+traviscult@users.noreply.github.com>
2022-10-15 18:26:30 -0500

    created task component and added react icon

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Task } from "./Task";
 
 export const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} />
       ))}
     </>
   );
