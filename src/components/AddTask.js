7c328edf4ae4211e9e85eec92bac6a3feaa53cc9
traviscult <60018617+traviscult@users.noreply.github.com>
2022-11-26 15:35:20 -0600

    added addtask form to app js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,7 +1,21 @@
-import React from "react";
-
 const AddTask = () => {
-  return <div>AddTask</div>;
+  return (
+    <form className="add-form">
+      <div className="form-control">
+        <label>Task</label>
+        <input type="text" placeholder="Add Task"></input>
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input type="text" placeholder="Add Day & Time"></input>
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input type="checkbox"></input>
+      </div>
+      <input type="submit" value="Save Task" className="btn btn-block"></input>
+    </form>
+  );
 };
 
 export default AddTask;
