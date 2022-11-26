d7b17eaf54b8679d824f310e43a7e8d100e1d5dc
traviscult <60018617+traviscult@users.noreply.github.com>
2022-11-26 15:25:10 -0600

    added reminder functionality on double click of tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Task } from "./Task";
 
-export const Tasks = ({ tasks, onDelete }) => {
+export const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete} />
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
