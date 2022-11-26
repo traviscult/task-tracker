62796f8a5f4a6e2e286e1cca4484f59da9c693f5
traviscult <60018617+traviscult@users.noreply.github.com>
2022-11-25 19:59:56 -0600

    added delete functionality to red x's

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
 
-export const Task = ({ task }) => {
+export const Task = ({ task, onDelete }) => {
   return (
     <div className="task">
       <h3>
-        {task.text} <FaTimes style={{ color: "red", cursor: "pointer" }} />
+        {task.text}{" "}
+        <FaTimes
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
       </h3>
       <p>{task.day}</p>
     </div>
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Task } from "./Task";
 
-export const Tasks = ({ tasks }) => {
+export const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
       {tasks.map((task) => (
-        <Task key={task.id} task={task} />
+        <Task key={task.id} task={task} onDelete={onDelete} />
       ))}
     </>
   );
